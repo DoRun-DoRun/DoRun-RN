27a447c5a8d48a356e897c137b92874b84ef164c
Cheonse <108046230+cheonseunghyeon@users.noreply.github.com>
2023-11-03 20:56:28 +0900

    Feat - 모달 프레임 제작 완료 (#6)

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,46 @@
 import React, {useState} from 'react';
-import {Button, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Button} from 'react-native';
+import Modals from './screens/Modal';
 
-import {
-  KakaoOAuthToken,
-  KakaoProfile,
-  KakaoProfileNoneAgreement,
-  getProfile,
-  logout,
-  login,
-  unlink,
-} from '@react-native-seoul/kakao-login';
-
-function App() {
-  const [result, setResult] = useState<string>('');
-
-  const signInWithKakao = async (): Promise<void> => {
-    try {
-      const token: KakaoOAuthToken = await login();
-      console.log(token);
-
-      setResult(JSON.stringify(token));
-    } catch (err) {
-      console.log(err);
-    }
+const App = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const pressButton = () => {
+    setModalVisible(true);
   };
 
-  const signOutWithKakao = async (): Promise<void> => {
-    try {
-      const message = await logout();
-      console.log(message);
-
-      setResult(message);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const getKakaoProfile = async (): Promise<void> => {
-    try {
-      const profile: KakaoProfile | KakaoProfileNoneAgreement =
-        await getProfile();
-      console.log(profile);
-
-      setResult(JSON.stringify(profile));
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const unlinkKakao = async (): Promise<void> => {
-    try {
-      const message = await unlink();
-      console.log(message);
-
-      setResult(message);
-    } catch (err) {
-      console.log(err);
-    }
-  };
   return (
-    <View>
-      <Text>result : {result}</Text>
-      <Button
-        testID="btn-login"
-        onPress={() => signInWithKakao()}
-        title={'카카오 로그인'}
-      />
-      <View style={{marginTop: 12}} />
-      <Button
-        testID="btn-login"
-        onPress={() => unlinkKakao()}
-        title={'링크 해제'}
-      />
-      <View style={{marginTop: 12}} />
-      <Button
-        testID="btn-login"
-        onPress={() => getKakaoProfile()}
-        title={'프로필 조회'}
-      />
-      <View style={{marginTop: 12}} />
-      <Button onPress={() => signOutWithKakao()} title={'카카오 로그아웃'} />
-      <View style={{marginTop: 40}} />
+    <View style={styles.container}>
+      <Button title={'모달 열기'} onPress={pressButton} />
+      <Modals modalVisible={modalVisible} setModalVisible={setModalVisible}>
+        <View style={styles.testcontainer}>
+          <Text style={styles.testtext}>View를 만들어서 모달을 제작</Text>
+          <Text style={styles.testtext}>1</Text>
+          <Text style={styles.testtext}>2</Text>
+          <Text style={styles.testtext}>1</Text>
+          <Text style={styles.testtext}>2</Text>
+          <Text style={styles.testtext}>1</Text>
+          <Text style={styles.testtext}>2</Text>
+          <Text style={styles.testtext}>1</Text>
+          <Text style={styles.testtext}>2</Text>
+        </View>
+      </Modals>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  testcontainer: {
+    gap: 5,
+  },
+  testtext: {
+    color: 'black',
+    fontSize: 25,
+  },
+});
 
 export default App;
