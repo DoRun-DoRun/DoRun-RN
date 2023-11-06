27132f6561094309c6cf0fee11400d73b21520af
joeunSong <thdwhdms99@naver.com>
2023-11-06 15:17:24 +0900

    Feat - CircleImage 컴포넌트 모듈화 (#)

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
+import {ImageSave} from '../components/ImageUpload';
 
 const MainScreen = () => {
   return (
     <View>
-      <Text>MainScreen</Text>
+      {/* <Text>MainScreen</Text> */}
+      <ImageSave />
     </View>
   );
 };
