e5b849a9f1c1a28d78c8384da0608fb5c71efa7f
joeunSong <thdwhdms99@naver.com>
2023-11-19 00:28:36 +0900

    Feat - 네비게이션 컴포넌트 생성 (#8)

--- src/Tab/RaceTab.tsx
+++ src/Tab/RaceTab.tsx
@@ -1,28 +1,67 @@
 import React from 'react';
-import {Pressable, Text} from 'react-native';
-import {Pressable, Text} from 'react-native';
+import {Pressable, Text, View} from 'react-native';
 import {HomeContainer} from '../Component';
 import {useNavigation} from '@react-navigation/native';
-import {useNavigation} from '@react-navigation/native';
+import OcticonIcons from 'react-native-vector-icons/Octicons';
+import styled from 'styled-components';
 
 const RaceTab = () => {
-  const navigation = useNavigation();
-
   return (
     <HomeContainer>
       <Text>RaceTab</Text>
-      <Pressable
-        onPress={() => navigation.navigate('FriendScreen' as never)}
-        android_ripple={{color: '#eeeeee'}}>
-        <Text>친구리스트 이동</Text>
-      </Pressable>
-      <Pressable
-        onPress={() => navigation.navigate('SettingScreen' as never)}
-        android_ripple={{color: '#eeeeee'}}>
-        <Text>환경설정 페이지 이동</Text>
-      </Pressable>
+
+      <Navigation />
     </HomeContainer>
   );
 };
 
+const NavigationButton = styled(View)`
+  width: 40px;
+  height: 40px;
+  background-color: ${props => props.theme.white};
+  /* iOS: 버튼에 그림자가 잘 보이는지 체크 */
+  /* shadow-color: #000;
+  shadow-offset: {
+    width: 0;
+    height: 2;
+  }
+  shadow-opacity: 0.25;
+  shadow-radius: 3.84; */
+  elevation: 5;
+  border-radius: 20px;
+  align-items: center;
+  justify-content: center;
+`;
+
+const NavigationContainer = styled(View)`
+  position: absolute;
+  top: 20px;
+  right: 16px;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+const Navigation = () => {
+  const navigation = useNavigation();
+
+  return (
+    <NavigationContainer>
+      <Pressable
+        onPress={() => navigation.navigate('FriendScreen' as never)}
+        android_ripple={{color: '#eeeeee'}}>
+        <NavigationButton>
+          <OcticonIcons name="people" size={24} />
+        </NavigationButton>
+      </Pressable>
+
+      <Pressable
+        onPress={() => navigation.navigate('SettingScreen' as never)}
+        android_ripple={{color: '#eeeeee'}}>
+        <NavigationButton>
+          <OcticonIcons name="gear" size={24} />
+        </NavigationButton>
+      </Pressable>
+    </NavigationContainer>
+  );
+};
 export default RaceTab;
