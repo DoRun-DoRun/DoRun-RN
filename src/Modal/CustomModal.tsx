a3ba4ce6b0f38c8268e5c8530eafe989cb4a428e
Cheonse <108046230+cheonseunghyeon@users.noreply.github.com>
2023-11-21 02:41:56 +0900

    Feat - 모달 구현 완료 (#6)

--- src/Modal/CustomModal.tsx
+++ src/Modal/CustomModal.tsx
@@ -1,52 +1,37 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, {FC, useEffect, useRef} from 'react';
-import {
-  Modal,
-  View,
-  Text,
-  TouchableOpacity,
-  Animated,
-  PanResponder,
-} from 'react-native';
+import {Modal, View, Animated, PanResponder} from 'react-native';
 import styled from 'styled-components/native';
 import {useModal} from './ModalProvider';
+import OverlayContainer from './OverlayContainer';
 
 const StyledModalContainer = styled(View)`
   flex: 1;
-  justify-content: center;
   align-items: center;
+  justify-content: flex-end;
 `;
 
 const StyledModalContent = styled(View)`
-  background-color: white;
-  padding: 20px;
+  padding: 16px;
+  padding-bottom: 0px;
   border-radius: 8px;
+  align-items: center;
 `;
 
-const StyledModalText = styled(Text)`
-  font-size: 16px;
-  margin-bottom: 10px;
-`;
-
-const StyledButton = styled(TouchableOpacity)`
-  background-color: #3498db;
-  padding: 10px;
-  border-radius: 5px;
-`;
-
-const StyledButtonText = styled(Text)`
-  color: white;
-  font-size: 16px;
+const AnimatedBottomSheet = styled(Animated.View)`
+  padding: 16px;
+  padding-bottom: 0px;
 `;
 
 const CustomModal: FC = () => {
   const {isVisible, content, hideModal} = useModal();
+  const panY = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
     if (isVisible) {
-      // Handle modal visibility changes or perform other actions
+      panY.setValue(0);
     }
-  }, [isVisible]);
-  const panY = useRef(new Animated.Value(0)).current;
+  }, [isVisible, panY]);
 
   const panResponders = useRef(
     PanResponder.create({
@@ -59,37 +44,37 @@ const CustomModal: FC = () => {
         if (gestureState.dy > 0 && gestureState.vy > 0.6) {
           hideModal();
         } else {
-          resetBottomSheet.start();
+          resetBottomSheet();
         }
       },
     }),
   ).current;
 
-  const resetBottomSheet = Animated.timing(panY, {
-    toValue: 0,
-    duration: 300,
-    useNativeDriver: true,
-  });
+  const resetBottomSheet = () => {
+    Animated.timing(panY, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
 
   return (
     <Modal
-      animationType="slide"
+      animationType={'fade'}
       transparent={true}
       visible={isVisible}
       onRequestClose={() => hideModal()}>
-      <StyledModalContainer>
-        <Animated.View
-          style={{transform: [{translateY: panY}]}}
-          {...panResponders.panHandlers}>
-          <StyledModalContent>
-            <StyledModalText>Custom Modal Content</StyledModalText>
-            {content}
-            <StyledButton onPress={() => hideModal()}>
-              <StyledButtonText>Close Modal</StyledButtonText>
-            </StyledButton>
-          </StyledModalContent>
-        </Animated.View>
-      </StyledModalContainer>
+      <OverlayContainer>
+        <StyledModalContainer>
+          <AnimatedBottomSheet
+            style={{
+              transform: [{translateY: panY}],
+            }}
+            {...panResponders.panHandlers}>
+            <StyledModalContent>{content}</StyledModalContent>
+          </AnimatedBottomSheet>
+        </StyledModalContainer>
+      </OverlayContainer>
     </Modal>
   );
 };
--- src/Modal/ExampleUsage.tsx
+++ src/Modal/ExampleUsage.tsx
@@ -3,17 +3,36 @@ import {View, Text, TouchableOpacity} from 'react-native';
 import {useModal} from './ModalProvider';
 import CustomModal from './CustomModal';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import styled from 'styled-components/native';
+import {Dimensions} from 'react-native';
 
+const windowWidth = Dimensions.get('window').width;
+
+const StyledModalContent = styled(View)`
+  background-color: white;
+  width: ${windowWidth * 0.9}px;
+  padding: 16px;
+  margin: 16px;
+  border-radius: 8px;
+`;
 const ExampleUsage = () => {
   const {showModal} = useModal();
 
   const openModal = () => {
     const modalContent = (
       <View>
-        <Text>모달 작성</Text>
-        <TouchableOpacity onPress={() => console.log('Button Pressed')}>
-          <Text>버튼 클릭</Text>
-        </TouchableOpacity>
+        <StyledModalContent>
+          <Text>내용 작성</Text>
+          <Text>내용 작성</Text>
+          <Text>내용 작성</Text>
+          <Text>내용 작성</Text>
+          <Text>내용 작성</Text>
+          <Text>내용 작성</Text>
+          <Text>내용 작성</Text>
+          <TouchableOpacity onPress={() => console.log('Button Pressed')}>
+            <Text>버튼 클릭</Text>
+          </TouchableOpacity>
+        </StyledModalContent>
       </View>
     );
 
