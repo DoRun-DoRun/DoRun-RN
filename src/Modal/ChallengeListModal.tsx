4086845153001d7d3bd2e00bba9cd481f0441da7
Cheonse <108046230+cheonseunghyeon@users.noreply.github.com>
2023-11-23 11:50:25 +0900

    Feat - 코드 수정 완료 (#19)

--- src/Modal/ChallengeListModal.tsx
+++ src/Modal/ChallengeListModal.tsx
@@ -1,15 +1,53 @@
 import React, {useState} from 'react';
 import {View, TouchableOpacity} from 'react-native';
-import {ButtonComponent, HomeContainer, NotoSansKR} from '../Component';
 import {
-  BetweenModalContainer,
-  ChallengeTimeBox,
-  ModalHeadText,
-  StatusComponent,
-} from './CustomModal';
+  ButtonComponent,
+  HomeContainer,
+  NotoSansKR,
+  TossFace,
+} from '../Component';
+import {ModalHeadText} from './CustomModal';
 import {useModal} from './ModalProvider';
 import OcticonIcons from 'react-native-vector-icons/Octicons';
-
+import styled from 'styled-components/native';
+export const BetweenModalContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 16px;
+  align-items: center;
+`;
+export const ChallengeTimeBox = styled.Text`
+  width: 100%;
+  border-radius: 25px;
+  border: 2px solid lightgray;
+  padding: 12px 40px;
+  align-items: center;
+  justify-content: center;
+`;
+export const StatusComponent = ({
+  username,
+  status,
+}: {
+  username: String;
+  status: String;
+}) => {
+  return (
+    <BetweenModalContainer>
+      <NotoSansKR size={17} weight="Medium">
+        {username}
+      </NotoSansKR>
+      {status === 'true' ? (
+        <NotoSansKR size={16} weight="Medium" color="green">
+          참여중이에요
+        </NotoSansKR>
+      ) : (
+        <NotoSansKR size={16} weight="Medium" color="red">
+          참여를 기다리고 있어요
+        </NotoSansKR>
+      )}
+    </BetweenModalContainer>
+  );
+};
 export const ChallengeListModal = () => {
   const [isSecondSectionVisible, setSecondSectionVisible] = useState(false);
   return (
@@ -22,16 +60,14 @@ export const ChallengeListModal = () => {
 
       <View style={{gap: 16}}>
         <View style={{gap: 16, flexDirection: 'row', alignItems: 'center'}}>
-          <NotoSansKR size={40} weight="Medium">
-            🎲
-          </NotoSansKR>
-          <NotoSansKR size={25}>챌린지 목표</NotoSansKR>
+          <TossFace size={40}>🎲</TossFace>
+          <NotoSansKR size={20}>챌린지 목표</NotoSansKR>
         </View>
 
         <TouchableOpacity
           onPress={() => setSecondSectionVisible(!isSecondSectionVisible)}>
           <BetweenModalContainer>
-            <NotoSansKR size={20}>챌린지 참여 인원</NotoSansKR>
+            <NotoSansKR size={18}>챌린지 참여 인원</NotoSansKR>
             <OcticonIcons name="chevron-down" size={26} color={'black'} />
           </BetweenModalContainer>
         </TouchableOpacity>
@@ -42,7 +78,7 @@ export const ChallengeListModal = () => {
             <StatusComponent username="닉네임 A" status="false" />
           </View>
         )}
-        <NotoSansKR size={20}>챌린지 기간</NotoSansKR>
+        <NotoSansKR size={18}>챌린지 기간</NotoSansKR>
         <ChallengeTimeBox>
           <NotoSansKR size={16} weight="Medium">
             2023. 10. 20(금) ~ 2024. 01. 01(월)
