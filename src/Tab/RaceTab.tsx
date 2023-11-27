12104b3ec8ea118d7a91400dfcae57a92861abdc
joeunSong <thdwhdms99@naver.com>
2023-11-27 18:01:45 +0900

    Feat - race page UI 구현 (#8)

--- src/Tab/RaceTab.tsx
+++ src/Tab/RaceTab.tsx
@@ -1,18 +1,59 @@
 import React from 'react';
-import {Pressable, Text, View} from 'react-native';
-import {HomeContainer} from '../Component';
+import {Pressable, View} from 'react-native';
+import {HomeContainer, ScrollContainer} from '../Component';
 import {useNavigation} from '@react-navigation/native';
 import OcticonIcons from 'react-native-vector-icons/Octicons';
-import styled from 'styled-components';
+import styled from 'styled-components/native';
 
 const RaceTab = () => {
   return (
     <HomeContainer>
-      <Text>RaceTab</Text>
-      <Navigation />
+      <ScrollContainer>
+        <BGComponent BGN={0} />
+        <BGComponent BGN={1} />
+        <BGComponent BGN={2} />
+        <BGComponent BGN={3} />
+        <BGComponent BGN={1} />
+        <BGComponent BGN={2} />
+        <Navigation />
+      </ScrollContainer>
     </HomeContainer>
   );
 };
+
+const BGComponent = ({BGN}: {BGN: number}) => {
+  const BGA = [
+    {url: require('../../assets/images/BGAHeader1.png'), height: 176},
+    {url: require('../../assets/images/BGABody1.png'), height: 137},
+    {url: require('../../assets/images/BGABody2.png'), height: 137},
+    {url: require('../../assets/images/BGABody3.png'), height: 137},
+  ];
+  const Dudus = [
+    require('../../assets/images/dudu00.png'),
+    require('../../assets/images/nuts00.png'),
+    require('../../assets/images/pachi00.png'),
+    require('../../assets/images/peats00.png'),
+  ];
+  return (
+    <View style={{flex: 1}}>
+      <BGAStyle source={BGA[BGN].url} aspect-ratio={1} height={BGA[BGN].height}>
+        <DuduImage source={Dudus[BGN]} />
+      </BGAStyle>
+    </View>
+  );
+};
+
+const DuduImage = styled.Image`
+  position: absolute;
+  left: 10;
+  bottom: 16;
+`;
+
+const BGAStyle = styled.ImageBackground<{height: number}>`
+  flex: 1;
+  height: ${props => props.height}px;
+  width: 100%;
+`;
 
 const NavigationButton = styled(View)`
   width: 40px;
@@ -21,7 +62,7 @@ const NavigationButton = styled(View)`
   shadow-color: #000;
   shadow-offset: 0px 2px;
   shadow-opacity: 0.25;
-  shadow-radius: 3.84;
+  shadow-radius: 3.84px;
   elevation: 5;
   border-radius: 20px;
   align-items: center;
@@ -34,6 +75,7 @@ const NavigationContainer = styled(View)`
   right: 16px;
   flex-direction: column;
   gap: 12px;
+  z-index: 10;
 `;
 
 const Navigation = () => {
