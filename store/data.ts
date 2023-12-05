4e8fe614850202a04a7eae5f9b58a48a258bbd1f
hin6150 <hin6150@gmail.com>
2023-12-05 13:24:11 +0900

    Feat - enum type 추가

---
+++ store/data.ts
@@ -0,0 +1,28 @@
+export enum SignType {
+  KAKAO = 'KAKAO',
+  APPLE = 'APPLE',
+  GUEST = 'GUEST',
+}
+
+export enum ChallengeStatusType {
+  PENDING = 'PENDING',
+  PROGRESS = 'PROGRESS',
+  COMPLETE = 'COMPLETE',
+  FAILED = 'FAILED',
+}
+
+export enum InviteAcceptType {
+  PENDING = 'PENDING',
+  ACCEPTED = 'ACCEPTED',
+  DECLINED = 'DECLINED',
+  DELETED = 'DELETED',
+}
+export enum ItemType {
+  BOOM = 'BOOM',
+  HAMMER = 'HAMMER',
+}
+
+export enum AvatarType {
+  CHARACTER = 'CHARACTER',
+  PET = 'PET',
+}
